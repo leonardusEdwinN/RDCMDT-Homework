ef4d47bee6e5efb0e5d99723b3106ee067ed1142
Edwin <osproject78@gmail.com>
2022-01-30 18:54:32 +0700

    Create Initial Project

---
+++ RDCMDT-Homework/RDCMDT-HomeworkUITests/RDCMDT_HomeworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RDCMDT_HomeworkUITestsLaunchTests.swift
+//  RDCMDT-HomeworkUITests
+//
+//  Created by Edwin Niwarlangga on 30/01/22.
+//
+
+import XCTest
+
+class RDCMDT_HomeworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
